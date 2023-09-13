4773a2cad0baf35d71e572888eb3b08b9497225c
bricked <git@bricked.dev>
2023-09-13 20:26:54 +0200

    feat: add fastfetch

---
+++ fastfetch/config.ps1
@@ -0,0 +1,14 @@
+<#
+.PARAMETER Name
+The destination of the config file.
+#>
+[CmdletBinding()]
+param (
+    [Parameter(Position = 1)]
+    [string]
+    $Destination = "$env:USERPROFILE/.config/fastfetch"
+)
+
+Write-Debug (New-Item $Destination -ItemType Directory -Force)
+Write-Output "Copying Fastfetch config..."
+Copy-Item "$PSScriptRoot/config.jsonc" -Destination $Destination
---
+++ fastfetch/install.ps1
@@ -0,0 +1,6 @@
+. "$PSScriptRoot/../scoop/install.ps1"
+
+Write-Output "Installing Fastfetch..."
+scoop install fastfetch
+
+. "$PSScriptRoot/config.ps1"
