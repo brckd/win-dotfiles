0a4142ca6727a9588417a25307194f14d5ac7070
bricked <git@bricked.dev>
2023-09-13 16:48:19 +0200

    feat: make installer work for any theme

--- theme/install.ps1
+++ theme/install.ps1
@@ -1,1 +1,13 @@
-Start-Process "$PSScriptRoot/purple.deskthemepack"
+<#
+.PARAMETER Name
+The name of the theme to install
+#>
+[CmdletBinding()]
+param (
+  [Parameter(Position = 1, Mandatory)]
+  [string]
+  $Name
+)
+
+
+Start-Process "$PSScriptRoot/$Name.deskthemepack"
